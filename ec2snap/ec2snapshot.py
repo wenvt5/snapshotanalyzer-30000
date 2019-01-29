8d00e59f8e538f4b5a68a05c5ba037fd9076f81d
Ying Wen <wenyingying@hotmail.com>
2019-01-29 00:42:03 -0500

    Don't start a snapshot if there's already one pending

--- ec2snap/ec2snapshot.py
+++ ec2snap/ec2snapshot.py
@@ -14,6 +14,10 @@ def filter_instances(project):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Auto manage snapshots"""
@@ -88,6 +92,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
 
